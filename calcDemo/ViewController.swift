98b470358b514cc684adf1d08be59f25ef4da9d1
Alex.Stepanoff <alex.stepanoff@mail.ru>
2016-09-23 10:45:30 +0300

    Initial Commit

---
+++ calcDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcDemo
+//
+//  Created by Алексей on 23.09.16.
+//  Copyright © 2016 Alex.Stepanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
